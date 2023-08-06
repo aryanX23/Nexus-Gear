ee8098fd5c143b9bff648d0db0a41727356c20a3
Aryan Rai <aryanx23@pop-os.localdomain>
2023-08-06 14:25:56 +0530

    adding stripe payment route

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,4 +1,6 @@
 const Users = require('../models/userModel');
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 async function getCart(req, res, next) {
     try {
@@ -22,7 +24,35 @@ async function getCart(req, res, next) {
         return res.send([]);
     }
 }
+async function handlePayment(req, res) {
+    try {
+        const { cartItems } = req.body;
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: cartItems.map((p) => {
+                return {
+                    name: p.name,
+                    description: p.description,
+                    amount: p.price * 100,
+                    currency: "inr",
+                    quantity: p.quantity,
+                };
+            }),
+            mode: "payment",
+            success_url:
+                req.protocol + "://" + req.get("host") + "/checkout/success",
+            cancel_url:
+                req.protocol + "://" + req.get("host") + "/checkout/cancel",
+        });
+        return res.send({ id: session.id, response: "Payment in Progress" });
+    }
+    catch (e) {
+        console.log(e);
+        return res.send({response: "Internal Error!"});
+    }
+};
 
 module.exports = {
-    getCart
+    getCart,
+    handlePayment
 }
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const cookieParser = require('cookie-parser');
-const { getCart } = require('../controllers/paymentController');
+const { getCart, handlePayment } = require('../controllers/paymentController');
 router.use(cookieParser());
 
+
 router.get('/getCart/:id', getCart);
+router.post("/create-checkout-session", handlePayment);
 
 module.exports = router
