7762a6be1fd8958fd91e739c27139c900ca477a2
aryanX23 <aryanrai2304@gmail.com>
2025-06-15 02:51:58 +0530

    refactor: simplify getId function by removing unnecessary try-catch block in generateId utility

--- apps/backend/utils/generateId.js
+++ apps/backend/utils/generateId.js
@@ -1,12 +1,8 @@
 const { v4: uuidv4 } = require("uuid");
 
 const getId = (prefix) => {
-  try {
     const suffix = uuidv4();
     return `${prefix}-${suffix}`;
-  } catch (err) {
-    throw err;
-  }
 };
 
 module.exports = { getId };
