6e2b377d68b20abb45a0bde1727dec1ecfdfa72f
aryanX23 <aryanrai2304@gmail.com>
2025-06-15 02:56:57 +0530

    refactor: update axios base URL to use environment variable for better configuration management in frontend API setup

--- apps/frontend/src/api/axios.js
+++ apps/frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000"; // TODO: shift to env level
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
 
 export default axios.create({
   baseURL: BASE_URL,
